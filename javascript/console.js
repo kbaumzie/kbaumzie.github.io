53b99cfde030e6b3c72a0e3d33f66953340e286b
kbaumzie <kayla.nussbaum@live.com>
2017-01-27 13:08:22 -0500

    Fix secure resource loading error with https

---
+++ javascript/console.js
@@ -0,0 +1,21 @@
+var hometown = "Kayla.hometown";
+var hometown2 = "Poconos, Pennsylvania";
+var education = "Kayla.education";
+var education2 = "Rochester Institute of Technology";
+var graduation = "Kayla.graduation";
+var graduation2 = "May, 2019";
+var degree = "Kayla.degree";
+var degree2 = "B.Sc. Software Engineering; Minor: Digital Literature and Comparative Media";
+var skills = "Kayla.skills";
+var skills2 = "['UI/UX', 'Technical Design', 'Project Management', 'Organization', 'Communication']";
+var techskills = "Kayla.skills.technical";
+var techskills2 = "['Ruby', 'Python', 'HTML/CSS', 'JavaScript', 'SQL', 'Java']";
+var experience = "Kayla.experience";
+var experience2 = "[{'Constant Contact': {'Position': 'Software Engineer Intern', \
+'Location': 'Waltham, MA', 'Duration': 'January 2017 - May 2017'}}, {'General Electric': \
+{'Position': 'Technical Developer Intern', 'Location': 'Feasterville-Trevose, PA', \
+'Duration': 'June 2016 - August 2016'}}]";
+var availability = "Kayla.availability.forHire";
+var availability2 = "['Fall 2017', 'Spring 2018', 'Summer 2019']";
+var contact = "Kayla.contact";
+var contact = "{'Email': 'kxn3867@rit.edu', 'Cell': '(803)460-5591'}";
