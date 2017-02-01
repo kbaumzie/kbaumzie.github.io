149479af01f4744aa81ba0518ff397c871fbb9a0
kbaumzie <kayla.nussbaum@live.com>
2017-02-01 00:19:32 -0500

    Added navbar that moves through div content

--- javascript/replacement_helper.js
+++
@@ -1,6 +0,0 @@
-// JavaScript helper for replacing content on main page
-
-function replaceContent(target, source) {
-  document.getElementById(target).innerHTML = document.getElementById(source).innerHTML;
-  // make fancy flash/transition when changing div content
-}
