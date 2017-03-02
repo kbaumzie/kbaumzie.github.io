769d612ea99b4c1f77294c5cce7c2a2f536bcb07
kbaumzie <kayla.nussbaum@live.com>
2017-03-02 13:37:42 -0500

    Adding to the console

--- javascript/console.js
+++ javascript/console.js
@@ -1,21 +1,56 @@
-var hometown = "Kayla.hometown";
-var hometown2 = "Poconos, Pennsylvania";
-var education = "Kayla.education";
-var education2 = "Rochester Institute of Technology";
-var graduation = "Kayla.graduation";
-var graduation2 = "May, 2019";
-var degree = "Kayla.degree";
-var degree2 = "B.Sc. Software Engineering; Minor: Digital Literature and Comparative Media";
-var skills = "Kayla.skills";
-var skills2 = "['UI/UX', 'Technical Design', 'Project Management', 'Organization', 'Communication']";
-var techskills = "Kayla.skills.technical";
-var techskills2 = "['Ruby', 'Python', 'HTML/CSS', 'JavaScript', 'SQL', 'Java']";
-var experience = "Kayla.experience";
-var experience2 = "[{'Constant Contact': {'Position': 'Software Engineer Intern', \
-'Location': 'Waltham, MA', 'Duration': 'January 2017 - May 2017'}}, {'General Electric': \
-{'Position': 'Technical Developer Intern', 'Location': 'Feasterville-Trevose, PA', \
-'Duration': 'June 2016 - August 2016'}}]";
-var availability = "Kayla.availability.forHire";
-var availability2 = "['Fall 2017', 'Spring 2018', 'Summer 2019']";
-var contact = "Kayla.contact";
-var contact = "{'Email': 'kxn3867@rit.edu', 'Cell': '(803)460-5591'}";
+var commands = [
+  {
+    input: 'Kayla.hometown',
+    output: 'Poconos, PA'
+  },
+  {
+    input: 'Kayla.education',
+    output: 'Rochester Institute of Technology'
+  }
+  {
+    input: 'Kayla.graduation',
+    output: 'May, 2019'
+  },
+  {
+    input: 'Kayla.degree',
+    output: 'B.Sc Software Engineering; Minor: Digital Literature and Comparative Media'
+  },
+  {
+    input: 'Kayla.skills',
+    output: "['UI/UX', 'Technical Design', 'Project Management', 'Organization', 'Communication']"
+  },
+  {
+    input: 'Kayla.programming',
+    output: "['Ruby', 'Python', 'HTML/CSS', 'JavaScript', 'SQL', 'Java']"
+  },
+  {
+    input: 'Kayla.experience',
+    output: "[{'Constant Contact': {'Position': 'Software Engineer Intern', \
+    'Location': 'Waltham, MA', 'Duration': 'January 2017 - May 2017'}}, {'General Electric': \
+    {'Position': 'Technical Developer Intern', 'Location': 'Feasterville-Trevose, PA', \
+    'Duration': 'June 2016 - August 2016'}}]"
+  },
+  {
+    input: 'Kayla.hire_availability',
+    output: "['Fall 2017', 'Spring 2018', 'Summer 2019']"
+  },
+  {
+    input: 'Kayla.contact_info',
+    output: "{'Email': 'kxn3867@rit.edu', 'Cell': '(803)460-5591'}"
+  }
+];
+
+function writeInput (str) {
+  //simulate typing
+  console.log(str);
+}
+
+function writeOutput (str) {
+  //instant print output
+  console.log(str);
+}
+
+commands.forEach(function(command) {
+  writeInput(command.input);
+  writeOutput(command.output);
+});
